bb0bdc55715a98ab71119f0dbffb298e6b51bfec
nasrielsidiq <mmuftinasrilsidiqmartono@gmail.com>
2025-07-29 11:02:40 +0700

    ignor config

--- utils/config.ts
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL = "http://103.13.206.4"; // ganti dengan URL API Anda
-
-export const ENDPOINTS = {
-    INTERNSHIPS: `${API_BASE_URL}/api/internships`,
-    COOKIES: `${API_BASE_URL}/sanctum/csrf-cookie/`,
-    LOGIN: `${API_BASE_URL}/api/login`,
-    REGISTER: `${API_BASE_URL}/api/register`,
-    SCHOOL_NAME: `${API_BASE_URL}/api/school_name`,
-}
